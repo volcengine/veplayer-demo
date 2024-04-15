5c118766bfefba28a00487c22897023e366574ca
xiongxiong.001 <xiongxiong.001@bytedance.com>
2024-04-16 01:43:23 +0800

    chore: fix build

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-rm -rf node_modules output
+
+set -e
+rm -rf packages/vod-h5-demo/output
 source ~/.nvm/nvm.sh
-nvm install 18
-nvm use 18
-npm install -g pnpm
+nvm install v18.16.0
+nvm use v18.16.0
+
+node -v
+npm -v
+npm i -g pnpm@7.16
+pnpm -v
+pnpm i --frozen-lockfile
 cd packages/vod-h5-demo
-pnpm install
+pnpm i
 pnpm build
-
-# yarn deploy
-
-
