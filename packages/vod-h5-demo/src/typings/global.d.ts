a2490190162336e75d90a95219b25803df6fae5f
wangguangbin <wangguangbin.2022@bytedance.com>
2024-05-17 14:11:41 +0800

    feat: update mp4 version

--- packages/vod-h5-demo/src/typings/global.d.ts
+++ packages/vod-h5-demo/src/typings/global.d.ts
@@ -1,13 +1,12 @@
 // TODO @byted -> @volcengine
 // import VePlayer from '@volcengine/veplayer';
 import VePlayer from '@/player';
-import { PreloaderManager } from '@byted/xgplayer-vod-preload';
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   interface Window {
     flexible: any;
     playerSdk: VePlayer;
-    preloader: PreloaderManager;
+    preloader: any;
   }
 }
