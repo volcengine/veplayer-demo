aa0de919ff37a68dbba8fe17996e28f8725a6f39
wangguangbin <wangguangbin.2022@bytedance.com>
2024-08-09 16:07:49 +0800

    feat: update volcengine version

--- packages/vod-h5-demo/src/player.ts
+++ packages/vod-h5-demo/src/player.ts
@@ -1,6 +1,6 @@
-import VePlayer, { Events } from '@byted/veplayer';
+import VePlayer, { Events } from '@volcengine/veplayer';
 // @ts-expect-error: 待添加type描述
-import mp4Encrypt from '@byted/veplayer/plugin/mp4Encrypt.js';
-import type { PlayerCore, IPlayerConfig } from '@byted/veplayer';
+import mp4Encrypt from '@volcengine/veplayer/plugin/mp4Encrypt.js';
+import type { PlayerCore, IPlayerConfig } from '@volcengine/veplayer';
 
 export { VePlayer as default, Events, PlayerCore, IPlayerConfig, mp4Encrypt };
