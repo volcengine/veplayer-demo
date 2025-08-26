1e8b8e402c1575405ab949b9f796154a303a5033
liangchang <liangchang@bytedance.com>
2025-08-26 11:27:07 +0800

    feat: touchStartPreventDefault={false} 支持PC点击进度条
    (cherry picked from commit bd343618cb314991f2751965efb031d1c879595e)

--- packages/vod-h5-demo/src/pages/square/index.tsx
+++ packages/vod-h5-demo/src/pages/square/index.tsx
@@ -157,6 +157,7 @@ function Square() {
         onTransitionEnd={() => {
           setIsSliderMoving(false);
         }}
+        touchStartPreventDefault={false}
       >
         <SwiperSlide>
           <div>
